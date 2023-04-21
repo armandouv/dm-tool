f3036f020dc966958276f1af3b808b16ecfa78e0
Armando Ugalde <armauv@gmail.com>
2023-04-20 19:07:38 -0600

    Add initial frontend files

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+import { NextUIProvider } from '@nextui-org/react';
+
+function App({ Component }) {
+  return (
+    <NextUIProvider>
+      <Component />
+    </NextUIProvider>
+  );
+}
+
+export default App;
