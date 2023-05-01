764148f1e3af88e02120a2e79fef58327b0c4e7f
Armando Ugalde <armauv@gmail.com>
2023-04-30 18:19:36 -0600

    Return 400 status code if error found

--- backend/app.py
+++ backend/app.py
@@ -14,11 +14,11 @@ CORS(app)
 def analisis_exploratorio():
     if not request.files.get("dataset"):
         print("No file attached")
-        return jsonify({'error': 'No se adjuntó el archivo de dataset'})
+        return jsonify({'error': 'No se adjuntó el archivo de dataset'}), 400
 
     df = csv_as_df(request.files["dataset"])
     if df is None:
-        return jsonify({'error': 'Error al leer archivo CSV'})
+        return jsonify({'error': 'Error al leer archivo CSV'}), 400
 
     result = obtener_analisis_exploratorio(df)
 
@@ -29,32 +29,32 @@ def analisis_exploratorio():
 def preparacion_datos():
     if not request.files.get("dataset"):
         print("No file attached")
-        return jsonify({'error': 'No se adjuntó el archivo de dataset'})
+        return jsonify({'error': 'No se adjuntó el archivo de dataset'}), 400
 
     df = csv_as_df(request.files["dataset"])
     if df is None:
-        return jsonify({'error': 'Error al leer archivo CSV'})
+        return jsonify({'error': 'Error al leer archivo CSV'}), 400
 
     json_data = request.form.get('json')
     if not json_data:
-        return jsonify({'error': 'No se proporcionó el objeto JSON'})
+        return jsonify({'error': 'No se proporcionó el objeto JSON'}), 400
     json_obj = json.loads(json_data)
 
     delete_nulls = json_obj.get('delete_nulls', False)
     if type(delete_nulls) is not bool:
-        return jsonify({'error': 'delete_nulls debe ser booleano'})
+        return jsonify({'error': 'delete_nulls debe ser booleano'}), 400
 
     attributes = json_obj.get('attributes', {})
     if type(attributes) is not dict:
-        return jsonify({'error': 'attributes debe ser un dict'})
+        return jsonify({'error': 'attributes debe ser un dict'}), 400
     for key in attributes.keys():
         value = attributes[key]
         if type(value) is not dict:
-            return jsonify({'error': 'attributes tiene una estructura invalida'})
+            return jsonify({'error': 'attributes tiene una estructura invalida'}), 400
 
     result = obtener_datos_preparados(df, delete_nulls, attributes)
 
-    return jsonify(result)
+    return jsonify(result), 400 if "error" in result else 200
 
 
 if __name__ == '__main__':
